82ab64e93e9f7c6e62edd95d5f7db7e61966e376
ArkadiusN <arek.nowacki4@gmail.com>
2022-11-29 14:46:52 +0000

    fixes

--- src/main/java/com/example/handlingformsubmission/WebSecurity.java
+++
@@ -1,6 +0,0 @@
-package com.example.handlingformsubmission;
-
-public class WebSecurity {
-    //TODO Go through the Amazon Cognito documentation to prepare
-    // user authorization via login form to access the register.
-}
